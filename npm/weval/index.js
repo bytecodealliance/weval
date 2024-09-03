e200fd1d06f41c1c658b1013de6154d4983e772d
Chris Fallin <chris@cfallin.org>
2024-09-03 14:25:26 -0700

    Version 0.2.11.

--- npm/weval/index.js
+++ npm/weval/index.js
@@ -10,7 +10,7 @@ import decompressTar from 'decompress-tar';
 import xz from '@napi-rs/lzma/xz';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const TAG = "v0.2.10";
+const TAG = "v0.2.11";
 
 async function getWeval() {
     const knownPlatforms = {
